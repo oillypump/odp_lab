95f865ffa9a1a78232c92b113635f9925affa730
oillypump <oillypump@gmail.com>
2025-12-18 12:24:47 +0700

    sesuaikan

--- metastore/entrypoint.sh
+++ metastore/entrypoint.sh
@@ -1,15 +1,4 @@
 #!/bin/bash
 set -e
 
-# Tunggu DB siap
-# /wait-for-it.sh metastore-db:5432 --timeout=60 --strict
-
-
-if /opt/hive/bin/schematool -dbType postgres -info 2>/dev/null | grep -q "Metastore schema version: 4.0.0"; then
-  echo "Schema already initialized"
-else
-  echo "Initializing Hive schema..."
-  /opt/hive/bin/schematool -dbType postgres -initSchema
-fi
-
 exec /opt/hive/bin/hive --service metastore
